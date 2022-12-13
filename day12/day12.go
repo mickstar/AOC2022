50248d209765b0cf451374bfa70e143ff510b8af
Michael Johnston <michael@datameshgroup.com>
2022-12-13 16:15:41 +1100

    added day 12

--- day12/day12.go
+++ day12/day12.go
@@ -17,13 +17,28 @@ func readFile(filename string) ([]string, error) {
 
 type Edge struct {
 	Node   *Node
-	Weight uint
+	Weight int
 }
 type Node struct {
 	X      uint
 	Y      uint
 	Height uint8
 	Edges  []Edge
+}
+
+func (n *Node) String() string {
+	var edgeString = "["
+	for _, edge := range n.Edges {
+		edgeString += fmt.Sprintf("Node(%d,%d)[%s], weight=%d", edge.Node.X, edge.Node.Y, string(edge.Node.Height), edge.Weight)
+		edgeString += ","
+	}
+	edgeString += "]"
+
+	return fmt.Sprintf("Node (%d,%d)[%s] with Edges %s", n.X, n.Y, string(n.Height), edgeString)
+}
+
+func (e *Edge) String() string {
+	return fmt.Sprintf("Edge[%s][%d]", e.Node, e.Weight)
 }
 
 // returns the head
@@ -56,40 +71,25 @@ func readGraph(data []string) (tree [][]*Node, start *Node, destination *Node) {
 			node := tree[y][x]
 			// we have to check around this node for things that we can move to
 
-			if x-1 >= 0 {
-				weight := calculateWeight(node.Height, tree[y][x-1].Height)
-				if weight == 1 {
-					node.Edges = append(node.Edges, Edge{
-						Node:   tree[y][x-1],
-						Weight: uint(weight),
-					})
-				}
+			directions := []struct {
+				x int
+				y int
+			}{
+				{0, -1}, // down
+				{0, 1},  // up
+				{-1, 0}, //left
+				{1, 0},  // right
 			}
-			if x+1 < len(tree[0]) {
-				weight := calculateWeight(node.Height, tree[y][x+1].Height)
-				if weight == 1 {
-					node.Edges = append(node.Edges, Edge{
-						Node:   tree[y][x+1],
-						Weight: uint(weight),
-					})
-				}
-			}
-			if y-1 >= 0 {
-				weight := calculateWeight(node.Height, tree[y-1][x].Height)
-				if weight == 1 {
-					node.Edges = append(node.Edges, Edge{
-						Node:   tree[y-1][x],
-						Weight: uint(weight),
-					})
-				}
-			}
-			if y+1 < len(tree) {
-				weight := calculateWeight(node.Height, tree[y+1][x].Height)
-				if weight == 1 {
-					node.Edges = append(node.Edges, Edge{
-						Node:   tree[y+1][x],
-						Weight: uint(weight),
-					})
+
+			for _, dir := range directions {
+				if y+dir.y >= 0 && y+dir.y < len(tree) && x+dir.x >= 0 && x+dir.x < len(tree[0]) {
+					weight := calculateWeight(node.Height, tree[y+dir.y][x+dir.x].Height)
+					if canMakeEdge(weight) {
+						node.Edges = append(node.Edges, Edge{
+							Node:   tree[y+dir.y][x+dir.x],
+							Weight: weight,
+						})
+					}
 				}
 			}
 		}
@@ -98,25 +98,36 @@ func readGraph(data []string) (tree [][]*Node, start *Node, destination *Node) {
 	return
 }
 
+func canMakeEdge(weight int) bool {
+	// allow going up 1 or down any amount
+	//return weight <= 1
+
+	//return weight == 0 || weight == -1 || weight <= 0
+	return weight >= -1
+}
+
 func calculateWeight(a, b uint8) int {
-	if a == 'z' && b == 'E' {
-		return 1
-	} else if a == 'S' && b == 'a' {
-		return 1
-	} else if a == 'S' || b == 'S' || a == 'E' || b == 'E' {
+	//if (a == 'z' || a == 'y') && b == 'E' {
+	//	return 1
+	//} else if a == 'S' && (b == 'a' || b == 'b') {
+	//	return 1
+	//}
+
+	if a == 'E' && (b == 'z' || b == 'y') {
 		return -1
 	}
 
-	val := int(b) - int(a)
-	if val == 0 {
-		return 1
+	if a == 'S' || b == 'S' || a == 'E' || b == 'E' {
+		return 100000
 	}
+
+	val := int(b) - int(a)
 	return val
 }
 
 // using Djikstras algo,
 // iterate over list of possible paths, take one then assess
-func shortestPath(tree [][]*Node, start *Node, dest *Node) int {
+func shortestPath(tree [][]*Node, start *Node, targetHeight uint8) int {
 	visited := make(map[*Node]bool)
 	distanceFromStart := make(map[*Node]int)
 
@@ -128,30 +139,32 @@ func shortestPath(tree [][]*Node, start *Node, dest *Node) int {
 	distanceFromStart[start] = 0
 
 	curNode := start
-	i := 0
 	for curNode != nil {
-		i++
 		fmt.Println("at node", curNode, "distance From start is ", distanceFromStart[curNode])
 		for _, edge := range curNode.Edges {
-			distanceForEdge := distanceFromStart[curNode] + int(edge.Weight)
-			if distanceFromStart[edge.Node] == -1 || distanceFromStart[edge.Node] > distanceForEdge {
+			distanceForEdge := distanceFromStart[curNode] + 1
+			if distanceFromStart[edge.Node] == -1 || distanceForEdge < distanceFromStart[edge.Node] {
 				distanceFromStart[edge.Node] = distanceForEdge
-			}
-
-			if edge.Node == dest {
-				return distanceFromStart[edge.Node]
 			}
 		}
 		visited[curNode] = true
 		curNode = getNextNodeToVisit(visited, distanceFromStart)
-		if curNode == nil {
-			fmt.Println("finished with ", len(visited), "visited and distances", distanceFromStart)
-		}
 	}
 
-	return distanceFromStart[dest]
+	minA := -1
+	for node, value := range distanceFromStart {
+		if node.Height == 'a' && value != -1 {
+			if minA == -1 || value < minA {
+				minA = value
+			}
+		}
+	}
+	return minA
 }
 
+// returns the next node to visit
+// returns nil if there are no more nodes to visit
+// from the list of unvisited nodes, we take one with the smallest distance from start
 func getNextNodeToVisit(visited map[*Node]bool, distances map[*Node]int) *Node {
 	var minNode *Node = nil
 	min := -1
@@ -176,6 +189,21 @@ func main() {
 
 	tree, start, dest := readGraph(data)
 
-	path := shortestPath(tree, start, dest)
-	fmt.Println("Shortest Path is ", path)
+	fmt.Println("start", start)
+	fmt.Println("dest", dest)
+
+	for y := 0; y < len(tree); y++ {
+		for x := 0; x < len(tree[0]); x++ {
+			if len(tree[y][x].Edges) == 0 {
+				fmt.Println("No edges for this node", tree[y][x])
+			}
+		}
+	}
+
+	////path := shortestPath(tree, start, dest)
+	//path := shortestPath(tree, start, 'E')
+	//fmt.Println("Part 1: Shortest Path is ", path)
+
+	path := shortestPath(tree, dest, 'a')
+	fmt.Println("Part 2: Shortest Path is ", path)
 }
